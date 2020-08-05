10f951624730d4bf4a56ce93236d38a7c0ed0c41
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-05 11:58:05 -0300

    first commit

---
+++ src/main/java/com/springbootkeycloak/codefiction/CodefictionDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootkeycloak.codefiction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefictionDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodefictionDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootkeycloak/codefiction/CodefictionDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootkeycloak.codefiction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefictionDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

78e0d56d2443dc905edfe3e5d7902a2d44eac59e
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-05 15:04:34 -0300

    app complete

--- src/main/java/com/springbootkeycloak/codefiction/User.java
+++ src/main/java/com/springbootkeycloak/codefiction/User.java
@@ -2,13 +2,11 @@ package com.springbootkeycloak.codefiction;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/springbootkeycloak/codefiction/UserDAO.java
+++ src/main/java/com/springbootkeycloak/codefiction/UserDAO.java
@@ -1,6 +1,8 @@
 package com.springbootkeycloak.codefiction;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserDAO extends CrudRepository<User, Long> {
+@Repository
+public interface UserDAO extends JpaRepository<User, Long> {
 }
